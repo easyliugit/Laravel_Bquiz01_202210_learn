c334ab3fa7dcbb55ea4fb238ec9017e953f3caa0
alphaliu <easy19050711@gmail.com>
2022-10-07 00:53:06 +0800

    完成後台內容

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,17 +3,17 @@
 @section('main')
     {{-- 選單 --}}
     <div class="menu col-3">
-        <div class="list-group">
-            <div class="border-bottom my-2 p-1 text-center">後台管理</div>
-            <div class="listgroup-item">標題圖片管理</div>
-            <div class="listgroup-item">動態廣告管理</div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
-            <div class="listgroup-item"></div>
+        <div class="list-group text-center">
+            <div class="border-bottom my-2 p-1">後台管理</div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/title" class="d-block">標題圖片管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/ad" class="d-block">動態文字廣告管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/image" class="d-block">校園映像圖片管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/mvim" class="d-block">動畫圖片管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/total" class="d-block">進站人數管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/bottom" class="d-block">頁尾版權管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/news" class="d-block">最新消息管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/admin" class="d-block">管理者管理</a></div>
+            <div class="list-group-item list-group-item-action px-0"><a href="/admin/menu" class="d-block">選單管理</a></div>
         </div>
         <div class="border text-center">訪客人數：</div>
     </div>

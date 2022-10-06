c26891f2274a1698aafe1fd5daa98ae5570e0053
alphaliu <easy19050711@gmail.com>
2022-10-07 01:46:47 +0800

    選單改用引入@include方式

--- README.md
+++ README.md
@@ -11,15 +11,18 @@
 - 使用Bootstrap 完成版型配置
 - 乙級題型一後台沒有首頁，重新調整路由路徑
 - 進階用法，連結同版面，在路由設定變數ex:http://localhost/admin/{name}，暫不使用Contrller方式
+- 選單改用引入@include方式
 - 
 - 2022/10/5
-- Laravel 前端模板Blade
+- Laravel 前端模板Blade，
 - 使用- Laravel 方便結合Vue.js 使用，提供在起始加上"@"可忽略原{{ }}"用法ex:@{{ $values }}
 - 使用asset()相對路徑找到css外部檔案
 - css 使用外部引入時，檔案須放置於public目錄下，並將路徑使用"{{ asset('css/style.css') }}"方法寫入，此方法Laravel 會使用相對路徑找到外部檔案
+- 套用@extends，套入@section=>@yield
 - 
 - 2022/10/4
 - 美克叔叔採用網頁設計乙級題組一作為範例來講解Laravel 的操作
 - 美克叔叔盡量用Laravel 4~8 都有的方法教學
 - 個人改用Laravel 9 學習
 - 安裝Laravel 需耐心等待，避免安裝不完全
+- 
---
+++ resources/views/layouts/backed_sidebar.blade.php
@@ -0,0 +1,15 @@
+<div class="menu col-3">
+    <div class="list-group text-center">
+        <div class="border-bottom my-2 p-1">後台管理</div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/title" class="d-block">標題圖片管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/ad" class="d-block">動態文字廣告管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/image" class="d-block">校園映像圖片管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/mvim" class="d-block">動畫圖片管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/total" class="d-block">進站人數管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/bottom" class="d-block">頁尾版權管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/news" class="d-block">最新消息管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/admin" class="d-block">管理者管理</a></div>
+        <div class="list-group-item list-group-item-action px-0"><a href="/admin/menu" class="d-block">選單管理</a></div>
+    </div>
+    <div class="border text-center">訪客人數：</div>
+</div>

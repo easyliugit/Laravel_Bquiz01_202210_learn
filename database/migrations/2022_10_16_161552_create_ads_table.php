0fa4955cb698423d6339f47150489c477d20b1da
alphaliu <easy19050711@gmail.com>
2022-10-17 01:04:46 +0800

    修改Ad 資料表型態

--- database/migrations/2022_10_16_161552_create_ads_table.php
+++ database/migrations/2022_10_16_161552_create_ads_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->string('text');
-            $table->boolean('sh');
+            $table->boolean('sh')->default(1);
             $table->timestamps();
         });
     }

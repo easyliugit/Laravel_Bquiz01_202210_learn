7fa56ec6f900419bbf090361b539c903eb16e35e
alphaliu <easy19050711@gmail.com>
2022-10-21 11:54:33 +0800

    完成版權管理功能

--- app/Http/Controllers/BottomController.php
+++ app/Http/Controllers/BottomController.php
@@ -36,4 +36,41 @@ class BottomController extends Controller
         ];
         return view('backend.module',$view);
     }
+
+    public function edit($id)
+    {
+        //
+        $bottom=Bottom::first();
+        $view=[
+            'action'=>'/admin/bottom/'.$id,
+            'method'=>'patch',
+            'modal_header'=>'頁尾版權管理',
+            'modal_body'=>[
+                [
+                    'label'=>'頁尾版權',
+                    'tag'=>'input',
+                    'type'=>'text',
+                    'name'=>'bottom',
+                    'value'=>$bottom->text,
+                ],
+            ],
+        ];
+
+        return view('modals.base_modal',$view);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+        $bottom=Bottom::find($id);
+
+        // 現在的資料已經會自動判斷這個動作
+        if ($bottom->text!=$request->input('bottom')) {
+            $bottom->text=$request->input('bottom');
+            $bottom->save();
+        }
+        
+
+        return redirect('/admin/bottom');
+    }
 }
